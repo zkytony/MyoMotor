333adc201ac765ae64591579227191c79b1fbebc
Kaiyu Zheng <qaz2wsx3@uw.edu>
2015-05-26 20:48:39 -0700

    It is a mistake for now to not have const config values

--- MyoMotor/config.h
+++ MyoMotor/config.h
@@ -2,14 +2,14 @@
 #define CONFIG_H
 
 namespace conf {
-	 int SAMPLE_SIZE = 8;
-	 int REFRESH_LIMIT = 500;
-	 int DURATION = 10; // how often does each "event loop" get executed
-	 int Y_MAX = 550;
-	 int MAX_POS_CLAW = 450;
-	 int MAX_POS_BASE = 800;
-	 int SENSITIVITY = 250;
-	 int GAP = 10;
+	 const int SAMPLE_SIZE = 8;
+	 const int REFRESH_LIMIT = 500;
+	 const int DURATION = 10; // how often does each "event loop" get executed
+	 const int Y_MAX = 550;
+	 const int MAX_POS_CLAW = 450;
+	 const int MAX_POS_BASE = 800;
+	 const int SENSITIVITY = 250;
+	 const int GAP = 10;
 }
 
 #endif
